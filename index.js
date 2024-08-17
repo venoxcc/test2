42e3918b744141edfa86719fd0bbe955c4c9c1d7
venox <167776494+venoxhh@users.noreply.github.com>
2024-08-17 20:45:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // Rate limiting
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 30, // limit each IP to 30 requests per minute
     message: 'Too many requests from this IP, please try again later.'
 });
 app.use(limiter);
